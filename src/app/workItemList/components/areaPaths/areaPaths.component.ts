3124913513ebe50ef18b7972bf7153b178e23133
PM Palles <petepalles@gmail.com>
2020-02-15 10:51:35 -0500

    refactor: flag of add button and add validation

--- src/app/workItemList/components/areaPaths/areaPaths.component.ts
+++ src/app/workItemList/components/areaPaths/areaPaths.component.ts
@@ -4,9 +4,9 @@ import _ from 'lodash';
 import { NestedTreeControl } from '@angular/cdk/tree';
 import { MatTreeNestedDataSource } from '@angular/material/tree';
 
-import {FormControl} from '@angular/forms';
-import {Observable} from 'rxjs';
-import {map, startWith} from 'rxjs/operators';
+import { FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 import * as SDK from "azure-devops-extension-sdk";
 import { IWorkItemFormNavigationService, WorkItemTrackingServiceIds } from "azure-devops-extension-api/WorkItemTracking";
 
@@ -23,7 +23,7 @@ interface AreaPathNode {
 
 export class AreaPathsComponent implements OnInit {
 
-    myControl = new FormControl();
+    myControl = new FormControl('', [Validators.required]);
     options: string[];
     filteredOptions: Observable<string[]>;
     flattenedArr = [];
@@ -85,5 +85,9 @@ export class AreaPathsComponent implements OnInit {
             "Microsoft.VSTS.Common.BusinessValue": 5,
             "Microsoft.VSTS.Common.ValueArea": 'Architectural'
          });
+    }
+
+    getErrorMessage() {
+        return this.myControl.hasError('required') ? 'You must enter an area path.' : '';
     }
 }
