6367f1d2347005e9fe8828d16a6c9939cdfbae27
Palles <wavemotionio@gmail.com>
2020-04-04 17:56:33 -0400

    feat: add search history guard

--- src/app/search/search-routing.module.ts
+++ src/app/search/search-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { SearchComponent } from './search.component';
+import { SearchHistoryGuard } from './search-history.guard';
 
 const routes: Routes = [
 		// {
@@ -11,7 +12,8 @@ const routes: Routes = [
 		// },
 		{
 	        path: '',
-	        component: SearchComponent
+	        component: SearchComponent,
+	        canActivate: [SearchHistoryGuard]
 	    }
 	];
 
