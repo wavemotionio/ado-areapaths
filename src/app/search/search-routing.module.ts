0c73d3a08a95331866bdf8281aadc361828d2236
Palles <wavemotionio@gmail.com>
2020-04-04 20:21:44 -0400

    refactor: remove unused constructs

--- src/app/search/search-routing.module.ts
+++ src/app/search/search-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { SearchComponent } from './search.component';
-import { SearchHistoryGuard } from './search-history.guard';
 
 const routes: Routes = [
 		{
 	        path: '',
 	        component: SearchComponent
-	        // canActivate: [SearchHistoryGuard]
 	    }
 	];
 
